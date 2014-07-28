1609a041ad513b629d2cec337b45a0cc3bd14354
Concetto Vecchio <info@cvsolutions.it>
2014-07-28 18:09:09 +0200

    prepare form ajax

--- routes/site.js
+++ routes/site.js
@@ -2,11 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    res.render('index', {});
+    res.render('site_index', {});
+});
+
+router.get('/categoria/:slug', function (req, res) {
+    res.render('site_category', {
+        name:req.param('slug')
+    });
+});
+
+router.get('/regione/:slug', function (req, res) {
+    res.render('site_region', {
+        name:req.param('slug')
+    });
 });
 
 router.get('/artista/:slug', function (req, res) {
-    res.render('detail_artist', {
+    res.render('site_artist', {
         name:req.param('slug')
     });
 });
