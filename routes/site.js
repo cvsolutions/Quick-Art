37f9f580e7ef9fd134d98e72c103d087202ef3ad
Concetto Vecchio <info@cvsolutions.it>
2014-07-28 18:52:52 +0200

    init layout

--- routes/site.js
+++ routes/site.js
@@ -5,6 +5,12 @@ router.get('/', function (req, res) {
     res.render('site_index', {});
 });
 
+router.get('/registrazione', function (req, res) {
+    res.render('site_registration', {
+        name:req.param('slug')
+    });
+});
+
 router.get('/categoria/:slug', function (req, res) {
     res.render('site_category', {
         name:req.param('slug')
