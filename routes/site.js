7f5213b34d60103f705ab90d048b81057fdf2f67
infomedia <concettovecchio@gmail.com>
2014-07-29 12:25:54 +0200

    prepare layout page

--- routes/site.js
+++ routes/site.js
@@ -7,37 +7,40 @@ router.get('/', function (req, res) {
 
 router.get('/artisti-contemporanei', function (req, res) {
     res.render('site_artists', {
-        name:req.param('slug')
+        name: req.param('slug')
     });
 });
 
 router.get('/quadri-opere-darte', function (req, res) {
     res.render('site_paintings_worksofart', {
-        name:req.param('slug')
+        name: req.param('slug'),
+        supplies: [, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     });
 });
 
 router.get('/registrazione', function (req, res) {
     res.render('site_registration', {
-        name:req.param('slug')
+        name: req.param('slug')
     });
 });
 
 router.get('/categoria/:slug', function (req, res) {
     res.render('site_category', {
-        name:req.param('slug')
+        name: req.param('slug'),
+        supplies: [, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     });
 });
 
 router.get('/regione/:slug', function (req, res) {
     res.render('site_region', {
-        name:req.param('slug')
+        name: req.param('slug')
     });
 });
 
 router.get('/artista/:slug', function (req, res) {
     res.render('site_artist', {
-        name:req.param('slug')
+        name: req.param('slug'),
+        supplies: [, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     });
 });
 
